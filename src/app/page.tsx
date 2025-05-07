797fc87a872e9ea1cec7951285cd611829e96ff9
Amirul Hossain <amirul1709@hotmail.com>
2025-05-07 17:21:20 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { BiHomeCircle } from "react-icons/bi";
 import { BiHash } from "react-icons/bi";
 import { BiBell } from "react-icons/bi";
@@ -37,7 +38,20 @@ export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center relative">
       <div className="max-w-screen-lg w-full h-full flex relative">
-        <section className="fixed w-72 flex flex-col">left sidebar</section>
+        <section className="fixed w-72 flex flex-col">
+          {navigation_items.map((item) => (
+            <Link
+              key={item.title}
+              className="bg-white/50 flex items-center justify-center space-x-2 rounded-3xl p-4"
+              href={`/${item.title.toLowerCase()}`}
+            >
+              <div>
+                <item.icon />
+              </div>
+              <div>{item.title}</div>
+            </Link>
+          ))}
+        </section>
         <main>home</main>
         <section>right sidebar</section>
       </div>
