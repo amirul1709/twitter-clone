d40d31dbd45a569cf7f7b98ce44e4878a68590c3
Amirul Hossain <amirul1709@hotmail.com>
2025-05-07 21:52:46 -0600

    leftsidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,11 @@
 import React from "react";
-import Link from "next/link";
-import {
-  BiHomeCircle,
-  BiHash,
-  BiBell,
-  BiEnvelope,
-  BiBookBookmark,
-  BiUser,
-} from "react-icons/bi";
-import { GrTwitter } from "react-icons/gr";
-import { BsThreeDots } from "react-icons/bs";
-
-const navigation_items = [
-  {
-    title: "Home",
-    icon: BiHomeCircle,
-  },
-  {
-    title: "Explore",
-    icon: BiHash,
-  },
-  {
-    title: "Notifications",
-    icon: BiBell,
-  },
-  {
-    title: "Messages",
-    icon: BiEnvelope,
-  },
-  {
-    title: "Bookmarks",
-    icon: BiBookBookmark,
-  },
-  {
-    title: "Profile",
-    icon: BiUser,
-  },
-];
+import LeftSidebar from "./components/LeftSidebar";
 
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center relative">
       <div className="max-w-screen-xl w-full h-full flex relative">
-        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
-          <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
-            <Link className="p-2 text-2xl py-2 px-6" href={"/"}>
-              <GrTwitter />
-            </Link>
-            {navigation_items.map((item) => (
-              <Link
-                key={item.title}
-                className="hover:bg-white/10 transition duration-200 flex items-center justify-start w-fit text-2xl space-x-4 rounded-3xl py-2 px-6"
-                href={`/${item.title.toLowerCase()}`}
-              >
-                <div>
-                  <item.icon />
-                </div>
-                <div>{item.title}</div>
-              </Link>
-            ))}
-            <button className="bg-primary hover:bg-primary/50 transition duration-200 w-full rounded-full m-4 p-4 text-2xl text-center">
-              Tweet
-            </button>
-          </div>
-          <button className="rounded-full flex items-center space-x-2 m-4 p-4 bg-transparent text-center hover:bg-white/10 transition duration-200 w-full justify-between">
-            <div className="flex items-center space-x-2">
-              <div className="rounded-full bg-slate-400 w-12 h-12"></div>
-              <div className="text-left text-sm">
-                <div className="font-semibold">Amirul Hossain</div>
-                <div>@amirul</div>
-              </div>
-            </div>
-            <div>
-              <BsThreeDots />
-            </div>
-          </button>
-        </section>
+        <LeftSidebar />
         <main>home</main>
         <section>right sidebar</section>
       </div>
