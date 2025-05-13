72a4a1ce57f3915b25c55c234de73ddf10a1eae4
Amirul Hossain <amirul1709@hotmail.com>
2025-05-13 13:49:08 -0600

    creating tweets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import LeftSidebar from "./components/LeftSidebar";
+import { BsDot } from "react-icons/bs";
 
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center relative">
       <div className="max-w-screen-xl w-full h-full flex relative">
         <LeftSidebar />
-        <main className="ml-[285px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600 p-6">
-          <h1 className="text-xl font-bold my-4">Home</h1>
-          <div className="border-t-[0.5px] border-b-[0.5px] border-gray-600 flex items-stretch py-4 space-x-2 h-35 relative">
-            <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
+        <main className="ml-[285px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600 px-6 py-2">
+          <h1 className="text-xl font-bold my-2">Home</h1>
+          <div className="border-t-[0.5px] border-b-[0.5px] border-gray-600 flex items-stretch py-4 space-x-2 relative">
+            <div className="w-11 h-11 bg-slate-400 rounded-full flex-none"></div>
             <div className="flex flex-col w-full h-full">
               <div className="border-gray-700">
                 <input
@@ -20,87 +21,51 @@ export default function Home() {
               </div>
               <div className="w-full justify-between items-center flex">
                 <div></div>
-                <div>
-                  <button className="bg-primary hover:bg-primary/50 transition duration-200 w-full rounded-full p-4 text-2xl text-center">
+                <div className="w-full max-w-[100px]">
+                  <button className="bg-primary hover:bg-primary/50 transition duration-200 w-full rounded-full px-4 py-2 text-lg font-bold text-center">
                     Tweet
                   </button>
                 </div>
               </div>
             </div>
           </div>
-          <p>
-            Thanos debuted in The Invincible Iron Man #55 (February 1973),
-            featuring a story by Jim Starlin that was co-scripted by Mike
-            Friedrich. The storyline from that issue continued through Captain
-            Marvel #25–33 (bi-monthly: March 1973 – Jan. 1974), Marvel Feature
-            #12 (Nov. 1973), Daredevil #107 (Jan. 1974), and Avengers #125 (July
-            1974). He returned in an extended storyline that spanned Strange
-            Tales #178–181 (Feb.–Aug. 1975), Warlock #9-11 (Oct. 1975 – Jan.
-            1976), Marvel Team Up #55 (March 1977), and the 1977 Annuals for
-            Avengers and Marvel Two-in-One (Thanos does not actually appear
-            until the end of Warlock #9). He was also featured in a short backup
-            story in Logan's Run #6 (June 1977) and had a small role in the
-            Death of Captain Marvel graphic novel (April 1982). The character
-            was revived in Silver Surfer vol. 3, #34 (Feb. 1990) and
-            guest-starred until issue #59 (November 1991), while simultaneously
-            appearing in The Thanos Quest #1–2 (Sept. – Oct. 1990) and The
-            Infinity Gauntlet #1–6 (July – Dec. 1991). After an appearance in
-            Spider-Man #17 (Dec. 1991), Thanos had a recurring role in Warlock
-            and the Infinity Watch #1–42 (Feb. 1992 – Aug. 1995). This was
-            followed by crossover appearances in Infinity War #1–6 (June – Nov.
-            1992), Infinity Crusade #1–6 (June – Nov. 1993), Silver Surfer vol.
-            3, #86–88 (Nov. 1993 – Jan. 1994), Warlock Chronicles #6–8, Thor
-            #468–471 (Nov. 1993 – Feb. 1994), Namor The Sub-Mariner #44 (Nov.
-            1993), Secret Defenders #11–14 (Jan. – April 1994), Cosmic Powers
-            #1–6 (March – July 1994), and Cosmic Powers Unlimited #1 (May 1995).
-            Thanos appeared in a connected storyline in Ka-Zar vol. 2, #4–11
-            (Aug. 1997 – March 1998), Ka-Zar Annual (1997), and the X-Man and
-            Hulk Annual (1998), before featuring in Thor vol. 2, #21–25 (March –
-            July 2000) and the 2000 Annual. The character was next used in
-            Captain Marvel vol. 4, #17–19 (June–Aug. 2001), Avengers: Celestial
-            Quest #1–8 (Nov. 2001 – June 2002), Infinity Abyss #1–6 (Aug. – Oct.
-            2002) and Marvel: The End #1–6 (May – Aug 2003). In 2004 Thanos
-            received an eponymous title that ran for 12 issues. In 2006, the
-            character played an important role in Annihilation: Silver Surfer
-            #1–4 (June – Sept. 2006) and Annihilation #1–6 (Oct. 2006 – March
-            2007). The character was re-introduced in Guardians of the Galaxy
-            vol. 2, #24–25 (April – May 2010) and played a major role in The
-            Thanos Imperative: Ignition (June 2010) and The Thanos Imperative
-            #1–6 (July – Dec. 2010). The character returned in Avengers Assemble
-            #1 (March 2012).[6] A mini-series titled Thanos: Son of Titan by Joe
-            Keatinge was planned for publication in August 2012, but was
-            cancelled.[7] The character's origin was expanded in the five-issue
-            Thanos Rising miniseries by Jason Aaron and Simone Bianchi which was
-            published monthly beginning in April 2013.[8] Later that same year,
-            Thanos played a central role in the Infinity miniseries written by
-            Jonathan Hickman and drawn by Jim Cheung, Jerome Opeña, and Dustin
-            Weaver. In May 2014, Jim Starlin and Ron Lim worked together on the
-            one-shot Thanos Annual, which is a prelude to a new trilogy of
-            original graphic novels. The first, Thanos: The Infinity Revelation,
-            was released the following August.[9][10] Beginning in February
-            2015, Starlin also penned a four-issue miniseries titled Thanos vs.
-            Hulk, which was set prior to the graphic novels. The second
-            installment in the trilogy, Thanos: The Infinity Relativity, was
-            released in June 2015.[11] The third graphic novel, Thanos: The
-            Infinity Finale, as well as the connected mini-series The Infinity
-            Entity were published in 2016.[12] At the same time Starlin was
-            writing these graphic novels and tie-ins, the character also
-            appeared in New Avengers #23–24 (Oct – Nov 2014),[13] Guardians of
-            the Galaxy vol. 3, #18–20 (Oct – Dec 2014), Legendary Star-Lord #4
-            (Dec 2014), a six-issue miniseries titled Thanos: A God Up There
-            Listening (Dec 2014), Avengers vol. 5, #40–41 (Mar – Apr 2015), and
-            Deadpool vol. 3, #45 ("#250") (Jun 2015). Thanos also played a major
-            role in the five-issue miniseries The Infinity Gauntlet vol. 2,
-            (July 2015 – Jan 2016), a tie-in of the cross-over Secret Wars
-            (2015). In 2017, as part of Marvel NOW!, Thanos received his own
-            solo title written by Jeff Lemire and drawn by Mike Deodato. After
-            11 issues, Donny Cates and Geoff Shaw took over as the creative
-            team. This storyline crossed over with Donny Cates' Cosmic Ghost
-            Rider storyline. In 2021, Thanos was later reintroduced as a primary
-            antagonist in the comic series Eternals vol. 5, #1–12, by the writer
-            Kieron Gillen and artist Esad Ribić, and plays a prominent role in
-            exploring Eternals mythos and retcons.
-          </p>
+          <div className="flex flex-col">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="border-b-[0.5px] border-gray-600 p-4 flex space-x-4"
+              >
+                <div>
+                  <div className="w-10 h-10 bg-slate-200 rounded-full"></div>
+                </div>
+                <div className="flex flex-col space-y-4">
+                  <div className="flex items-center space-x-1">
+                    <div className="font-bold">Amirul Hossain</div>
+                    <div>@amirul1709</div>
+                    <div>
+                      <BsDot />
+                    </div>
+                    <div>1 hour ago</div>
+                  </div>
+                  <div className="text-white text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente atque sunt ipsum quia veritatis. Deleniti nisi
+                    inventore dolorum quibusdam, ut, dignissimos impedit ullam
+                    facilis sequi reprehenderit libero. Voluptatibus, reiciendis
+                    eligendi?
+                  </div>
+                  <div className="bg-slate-400 aspect-square w-full h-96 rounded-2xl"></div>
+                  <div className="flex items-center space-x-2 w-full">
+                    <div>C</div>
+                    <div>R</div>
+                    <div>L</div>
+                    <div>S</div>
+                    <div>SH</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </main>
         <section>RIGHT SIDEBAR</section>
       </div>
