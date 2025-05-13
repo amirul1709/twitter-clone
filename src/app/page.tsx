8a5fc73b42232ee9ca67548b0cf615ee3835ef94
Amirul Hossain <amirul1709@hotmail.com>
2025-05-13 12:55:33 -0600

    drafting tweet section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,27 @@ export default function Home() {
       <div className="max-w-screen-xl w-full h-full flex relative">
         <LeftSidebar />
         <main className="ml-[285px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600 p-6">
-          <h1 className="text-2xl font-bold">Home</h1>
+          <h1 className="text-xl font-bold my-4">Home</h1>
+          <div className="border-t-[0.5px] border-b-[0.5px] border-gray-600 flex items-stretch py-4 space-x-2 h-35 relative">
+            <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
+            <div className="flex flex-col w-full h-full">
+              <div className="border-gray-700">
+                <input
+                  className="w-full h-full text-2xl placeholder:text-gray-600 border-b-[0.5px] bg-transparent outline-none border-none"
+                  type="text"
+                  placeholder="What's happening?"
+                />
+              </div>
+              <div className="w-full justify-between items-center flex">
+                <div></div>
+                <div>
+                  <button className="bg-primary hover:bg-primary/50 transition duration-200 w-full rounded-full p-4 text-2xl text-center">
+                    Tweet
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <p>
             Thanos debuted in The Invincible Iron Man #55 (February 1973),
             featuring a story by Jim Starlin that was co-scripted by Mike
