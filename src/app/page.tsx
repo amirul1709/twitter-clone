d9eb815deec8839ca363a63d17675e1d69da279c
Amirul Hossain <amirul1709@hotmail.com>
2025-05-14 13:04:50 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,18 +12,18 @@ export default function Home() {
         <section className="sticky top-0 w-[30%] flex flex-col items-stretch h-screen px-4">
           <div className="mt-4">
             <div className="relative w-full h-full">
-              <label
-                className="absolute top-0 left-0 h-full flex items-center justify-center p-4"
-                htmlFor="searchBox"
-              >
-                <BsSearch className="w-5 h-5 text-gray-500" />
-              </label>
               <input
-                className="w-full h-full rounded-xl py-4 pl-11 pr-4 outline-none bg-neutral-900 border focus:border-primary focus:border"
+                className="w-full h-full rounded-xl py-4 pl-11 pr-4 outline-none bg-neutral-900 border-gray-600 border focus:border-primary focus:border peer"
                 id="searchBox"
                 type="text"
-                placeholder="Search..."
+                placeholder="Search Twitter"
               />
+              <label
+                className="absolute top-0 left-0 h-full flex items-center justify-center p-4 text-gray-500 peer-focus:text-primary"
+                htmlFor="searchBox"
+              >
+                <BsSearch className="w-5 h-5" />
+              </label>
             </div>
           </div>
           <div></div>
